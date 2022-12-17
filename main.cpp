6f2c3cd8a98ddc6856e671ae78ec526e6d9f0133
Advait <avnaik_m22@ce.vjti.ac.in>
2022-12-17 12:08:44 +0530

    My Changes

--- main.cpp
+++ main.cpp
@@ -5,5 +5,11 @@ using namespace std;
 
 int main(){
 	cout << "Hello, World!" << endl;
+
+	string name;
+	cout << "Enter your name: " << endl;
+	cin >> name;
+	cout << "Hello, " << name << endl;
+
 	return 0;
 }
